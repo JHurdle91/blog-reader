c324eced46c58a913cd8b99454df3351ba969227
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-10-07 13:50:54 -0400

    implement comment count on PostListItem

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,6 +1,16 @@
+import { useState, useEffect } from "react";
+
+import UserService from "../services/user.service";
+
 const PostListItem = (props) => {
   const { title, timestamp_formatted, text, _id } = props.post;
-  const nComments = 5;
+
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    UserService.getComments(_id).then((result) => {
+      setComments(result.data);
+    });
+  }, [_id]);
 
   const handleClick = () => {
     window.location.href = `/posts/${_id}`;
@@ -11,7 +21,11 @@ const PostListItem = (props) => {
       <h2>{title}</h2>
       <div>{timestamp_formatted}</div>
       <p>{text}</p>
-      <div>{nComments}</div>
+      {comments.length === 1 ? (
+        <div>1 comment</div>
+      ) : (
+        <div>{comments.length} comments</div>
+      )}
     </div>
   );
 };
