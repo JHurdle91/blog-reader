e64975dafa0752f8752a620ef9ca6256b1576a9e
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-10-20 20:29:34 -0400

    Updates

---
+++ precache-manifest.befaae9f4e3c5e063e38edc9be3ae6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b79f143aa7754ff4da73938ef0df11",
+    "url": "/blog-reader/index.html"
+  },
+  {
+    "revision": "56428240bcec1abb5971",
+    "url": "/blog-reader/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "311da7acfe3b5a7ad46f",
+    "url": "/blog-reader/static/css/main.7d2ed9e8.chunk.css"
+  },
+  {
+    "revision": "56428240bcec1abb5971",
+    "url": "/blog-reader/static/js/2.5589d8f1.chunk.js"
+  },
+  {
+    "revision": "3a6486b04a0ec1eb8d7a218fbfbd10ba",
+    "url": "/blog-reader/static/js/2.5589d8f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311da7acfe3b5a7ad46f",
+    "url": "/blog-reader/static/js/main.6c4c4877.chunk.js"
+  },
+  {
+    "revision": "4b66de7cf15cfbdcb0ab",
+    "url": "/blog-reader/static/js/runtime-main.2b20e127.js"
+  }
+]);
