21ce6a58dc29c21814c36ef2bd1bcbdfa9259a12
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-10-21 10:51:25 -0400

    remove url basename

--- src/Routes.js
+++ src/Routes.js
@@ -19,7 +19,7 @@ const Routes = () => {
   const profile = "/profile";
 
   return (
-    <BrowserRouter basename={PUBLIC_URL}>
+    <BrowserRouter>
       <Navbar />
       <Switch>
         <Route exact path={["/", "/home"]} component={App} />
